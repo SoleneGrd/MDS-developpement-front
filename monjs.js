93984593e7ea0ce853b80b0d7671fd86d14c3cb6
SoleneGrd <60424458+SoleneGrd@users.noreply.github.com>
2020-01-29 12:17:40 +0100

    troisième dépôt

--- monjs.js
+++ monjs.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $('h1').hide('slow');
+    //$('h1').hide(5000);
+
+    for ( var i = 0; i < 5; i++ ) {
+        $('<div class="col-1 boite">' ).appendTo( '#effets' );
+    }
 });

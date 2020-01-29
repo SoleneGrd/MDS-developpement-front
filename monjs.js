e5055d8900c5bf298cf9cf8272afadf42b51d6e1
SoleneGrd <60424458+SoleneGrd@users.noreply.github.com>
2020-01-29 12:22:25 +0100

    cinquième dépôt

--- monjs.js
+++ monjs.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     //$('h1').hide(5000);
 
     for ( var i = 0; i < 5; i++ ) {
-        $('<div class="col-1 boite">' ).appendTo( '#effets' );
+        $('<div class="col-1">' ).appendTo( '#effets' );
     }
 });
